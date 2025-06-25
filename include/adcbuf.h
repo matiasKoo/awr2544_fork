b44d98b3e1c0e66723da33f189af33a5d1752972
jerpism <jere.m@live.fi>
2025-06-25 13:30:36 +0300

    Comments to adcbuf.h and fix return type.

--- include/adcbuf.h
+++ include/adcbuf.h
@@ -1,4 +1,10 @@
 #ifndef ADCBUF_H
 #define ADCBUF_H
-void adcbuf_init(ADCBuf_Handle handle);
+
+/* Initialize the ADC Buffer 
+ * sets some default settings that work with the MMWave DFE
+ * handle will be set to the ADCBuf handle 
+ * returns 0 on success, <0 otherwise */
+int32_t adcbuf_init(ADCBuf_Handle handle);
+
 #endif /* ADCBUF_H */
