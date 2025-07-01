7ecf0a4c6a985329a824ebc7808bf10eded88a29
jerpism <jere.m@live.fi>
2025-07-01 12:38:51 +0300

    And the updated header...

--- c-src/include/adcbuf.h
+++ c-src/include/adcbuf.h
@@ -1,10 +1,12 @@
 #ifndef ADCBUF_H
 #define ADCBUF_H
 
+#include <drivers/adcbuf.h>
+
 /* Initialize the ADC Buffer 
  * sets some default settings that work with the MMWave DFE
  * handle will be set to the ADCBuf handle 
  * returns 0 on success, <0 otherwise */
-int32_t adcbuf_init(ADCBuf_Handle handle);
+ADCBuf_Handle adcbuf_init(void);
 
 #endif /* ADCBUF_H */
