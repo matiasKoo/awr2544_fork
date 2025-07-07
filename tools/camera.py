65dc001cc57643349811aca220089f41e2fac3ff
jerpism <jere.m@live.fi>
2025-07-07 10:56:51 +0300

    Add absolute abomination of a python script to read the serial console output and plot it.

---
+++ tools/camera.py
@@ -0,0 +1,69 @@
+import io
+import serial
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ser = serial.Serial(port='/dev/ttyACM0',baudrate=115200);
+
+range_res = 0.39
+
+f = plt.figure()
+plt.ion()
+plt.title("idk")
+plt.xlabel("Range (m)")
+plt.ylabel("This is the y axis")
+plt.grid(True)
+
+while True:
+    # This blocks until a stx is received, I hope?
+    # not that anything should be sent after init is done
+    ser.read_until(expected=b'\x02')
+
+
+    # And then read whatever is sent between STX and ETX
+    x = ser.read_until(expected=b'\x03')
+    
+    # convert to a string 
+    x = str(x, 'utf-8')
+
+    # strip ETX and comma from the end
+    x = x[:-2]
+
+    df = pd.read_csv(io.StringIO(x), header=None)
+
+    data = df.to_numpy(dtype=np.int16)
+    data = np.reshape(data,shape=(512,))
+
+
+    real = data[::2]
+    imag = data[1::2]
+
+
+    # needs to be dtype int32 since apparently no overflow check
+    real = np.square(real,dtype=np.int32)
+    imag = np.square(imag,dtype=np.int32)
+
+
+    out = np.add(real, imag)
+    out = np.sqrt(out)
+
+
+    print(out.shape)
+
+
+    ranges = np.arange(out.shape[0]) * range_res
+
+    # clear and replot every time
+    plt.clf()
+    plt.plot(ranges, out)
+    plt.show()
+    plt.pause(1)
+
+
+
+
+
+
+
+
