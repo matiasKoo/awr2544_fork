f625eff8996856b0e4b401bbb176350302026381
jerpism <jere.m@live.fi>
2025-07-15 13:53:14 +0300

    Gets everything ready for a connection but can't actually connect.

--- c-src/enet_project.c
+++ c-src/enet_project.c
@@ -12,7 +12,8 @@
 #include "lwip/sys.h"
 #include "lwip/tcpip.h"
 #include "lwip/dhcp.h"
-
+#include "lwip/sys.h"
+#include "lwip/api.h"
 #include <networking/enet/core/include/per/cpsw.h>
 #include <networking/enet/utils/include/enet_apputils.h>
 #include <networking/enet/utils/include/enet_board.h>
@@ -22,8 +23,85 @@
 #include "ti_enet_config.h"
 #include "ti_enet_lwipif.h"
 
+
+#define HOST_SERVER_IP6  ("FE80::12:34FF:FE56:78AB")
+
+#define HOST_SERVER_PORT  (8888)
+
+#define APP_MAX_RX_DATA_LEN (1024U)
+
+#define APP_NUM_ITERATIONS (2U)
+
+#define APP_SEND_DATA_NUM_ITERATIONS (5U)
+
+#define MAX_IPV4_STRING_LEN (20U)
+
+struct App_hostInfo_t
+{
+    ip_addr_t ipAddr;
+    uint16_t port;
+};
+
+char snd_buf[APP_MAX_RX_DATA_LEN];
 LwipifEnetApp_Handle hlwipIfApp = NULL;
 struct netif *g_pNetif[ENET_SYSCFG_NETIF_COUNT];
+
+static const uint8_t BROADCAST_MAC_ADDRESS[ENET_MAC_ADDR_LEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+static void AppTcp_fillHostSocketInfo(struct App_hostInfo_t* pHostInfo);
+
+static struct App_hostInfo_t gHostInfo;
+static char   gHostServerIp4[MAX_IPV4_STRING_LEN] = "";
+
+
+static void AppTcp_fillHostSocketInfo(struct App_hostInfo_t* pHostInfo)
+{
+    EnetAppUtils_print(" IP eneterd is: %s \r\n", gHostServerIp4);
+    int32_t addr_ok;
+    pHostInfo->port = HOST_SERVER_PORT;
+    memset(&pHostInfo->ipAddr, 0, sizeof(pHostInfo->ipAddr));
+    ip_addr_t*  pAddr = &pHostInfo->ipAddr;
+    IP_SET_TYPE_VAL(*pAddr, IPADDR_TYPE_V4);
+    addr_ok = ip4addr_aton(gHostServerIp4, ip_2_ip4(pAddr));
+    EnetAppUtils_assert(addr_ok);
+
+    return;
+}
+
+
+void EnetApp_addMCastEntry(Enet_Type enetType,
+                          uint32_t instId,
+                          uint32_t coreId,
+                          const uint8_t *testMCastAddr,
+                          uint32_t portMask)
+{
+    Enet_IoctlPrms prms;
+    int32_t status;
+    CpswAle_SetMcastEntryInArgs setMcastInArgs;
+    uint32_t setMcastOutArgs;
+
+    if (Enet_isCpswFamily(enetType))
+    {
+        Enet_Handle hEnet = Enet_getHandle(enetType, instId);
+
+        EnetAppUtils_assert(hEnet != NULL);
+        memset(&setMcastInArgs, 0, sizeof(setMcastInArgs));
+        memcpy(&setMcastInArgs.addr.addr[0U], testMCastAddr,
+               sizeof(setMcastInArgs.addr.addr));
+        setMcastInArgs.addr.vlanId  = 0;
+        setMcastInArgs.info.super = false;
+        setMcastInArgs.info.numIgnBits = 0;
+        setMcastInArgs.info.fwdState = CPSW_ALE_FWDSTLVL_FWD;
+        setMcastInArgs.info.portMask = portMask;
+        ENET_IOCTL_SET_INOUT_ARGS(&prms, &setMcastInArgs, &setMcastOutArgs);
+        ENET_IOCTL(hEnet, coreId, CPSW_ALE_IOCTL_ADD_MCAST, &prms, status);
+        if (status != ENET_SOK)
+        {
+           EnetAppUtils_print("EnetTestBcastMcastLimit_AddAleEntry() failed CPSW_ALE_IOCTL_ADD_MCAST: %d\n",
+                               status);
+        }
+    }
+}
 
 
 static inline int32_t App_isNetworkUp(struct netif* netif_)
@@ -238,11 +316,88 @@ static void App_setupNetworkStack()
     return;
 }
 
+void AppTcp_showMenu(void)
+{
+    ip_addr_t ipAddr;
+    int32_t addr_ok = 0;
+    EnetAppUtils_print(" TCP Client Menu: \r\n");
+
+    do
+    {
+        EnetAppUtils_print(" Enter TCP server IPv4 address:(example: 192.168.101.100)\r\n");
+        DebugP_scanf("%s", gHostServerIp4);
+        addr_ok = ip4addr_aton(gHostServerIp4, ip_2_ip4(&ipAddr));
+        TaskP_yield();
+    } while (addr_ok != 1);
+}
+
+static void AppTcp_simpleclient(void *pArg)
+{
+    struct netconn *pConn = NULL;
+    err_t err = ERR_OK, connectError = ERR_OK;
+    struct App_hostInfo_t* pHostInfo = (struct App_hostInfo_t*) pArg;
+    uint32_t buf_len = 0;
+    const enum netconn_type connType = NETCONN_TCP;
+
+    /* Create a new connection identifier. */
+    for (uint32_t pktIdx = 0; pktIdx < APP_NUM_ITERATIONS; pktIdx++)
+    {
+        struct netbuf *rxBbuf = NULL;
+        pConn = netconn_new(connType);
+        if (pConn != NULL)
+        {
+            /* Connect to the TCP Server */
+            EnetAppUtils_print("<<<< ITERATION %d >>>>\r\n", (pktIdx + 1));
+            EnetAppUtils_print(" Connecting to: %s:%d \r\n", gHostServerIp4, HOST_SERVER_PORT);
+            connectError = netconn_connect(pConn, &pHostInfo->ipAddr, pHostInfo->port);
+            if (connectError != ERR_OK)
+            {
+                netconn_close(pConn);
+                DebugP_log("Connection with the server isn't established error %s\r\n",lwip_strerr(connectError));
+                continue;
+            }
+
+            DebugP_log("Connection with the server is established\r\n");
+            // send the data to the server
+            for ( uint32_t i = 0; i < APP_SEND_DATA_NUM_ITERATIONS; i++)
+            {
+                memset(&snd_buf, 0, sizeof(snd_buf));
+                buf_len = snprintf(snd_buf, sizeof(snd_buf), "Hello over TCP %d", i+1);
+                err = netconn_write(pConn, snd_buf, buf_len, NETCONN_COPY);
+                if (err == ERR_OK)
+                {
+                    printf("\"%s\" was sent to the Server\r\n", snd_buf);
+                }
+                else
+                {
+                    DebugP_log("couldn't send packet to server\r\n");
+                    continue;
+                }
+
+                /* wait until the data is sent by the server */
+                if (netconn_recv(pConn, &rxBbuf) == ERR_OK)
+                {
+                    DebugP_log("Successfully received the packet %d\r\n", i+1);
+                    netbuf_delete(rxBbuf);
+                }
+                else
+                {
+                    DebugP_log("No response from server\r\n");
+                }
+            }
+            netconn_close(pConn);
+            netconn_delete(pConn);
+            DebugP_log("Connection closed\r\n");
+            ClockP_sleep(1);
+        }
+    }
+}
+
 
 void enet_test(void *args){
     int32_t ret = 0;
 
-    EnetAppUtils_enableClocks(ENET_CPSW_3G, 0);
+    EnetAppUtils_enableClocks(ENET_CPSW_2G, 0);
 
     DebugP_log("Initializing enet\r\n");
     EnetApp_driverInit();
@@ -259,6 +414,13 @@ void enet_test(void *args){
         DebugP_log("Handle is %p\r\n", handle);
     }
 
+
+    EnetApp_addMCastEntry(ENET_CPSW_2G,
+                        0,
+                        EnetSoc_getCoreId(),
+                        BROADCAST_MAC_ADDRESS,
+                        CPSW_ALE_ALL_PORTS_MASK);
+
     DebugP_log("Setting up network stack\r\n");
     App_setupNetworkStack();
 
@@ -270,7 +432,12 @@ void enet_test(void *args){
     }
 
     DebugP_log("network is UP!\r\n");
+    AppTcp_showMenu();
+    AppTcp_fillHostSocketInfo(&gHostInfo);
+    sys_thread_t ret2 = sys_thread_new("tcpinit_thread", AppTcp_simpleclient, &gHostInfo, DEFAULT_THREAD_STACKSIZE, DEFAULT_THREAD_PRIO);
+    DebugP_log("ret from thread %d\r\n",ret2);
 
+   // AppTcp_simpleclient(&gHostInfo);
 
-    while(1) __asm__ volatile("wfi");
+    vTaskDelete(NULL);
 }
