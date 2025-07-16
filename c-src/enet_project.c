0a9f3cae11ae33780259abdd18dd24d6706a39be
jerpism <jere.m@live.fi>
2025-07-16 12:06:35 +0300

    Separate the EnetApp functions to app_cpswconfighandler as done in the examples.

--- c-src/enet_project.c
+++ c-src/enet_project.c
@@ -22,6 +22,7 @@
 #include "ti_enet_open_close.h"
 #include "ti_enet_config.h"
 #include "ti_enet_lwipif.h"
+#include "app_cpswconfighandler.h"
 
 
 #define HOST_SERVER_IP6  ("FE80::12:34FF:FE56:78AB")
@@ -69,45 +70,11 @@ static void AppTcp_fillHostSocketInfo(struct App_hostInfo_t* pHostInfo)
 }
 
 
-void EnetApp_addMCastEntry(Enet_Type enetType,
-                          uint32_t instId,
-                          uint32_t coreId,
-                          const uint8_t *testMCastAddr,
-                          uint32_t portMask)
-{
-    Enet_IoctlPrms prms;
-    int32_t status;
-    CpswAle_SetMcastEntryInArgs setMcastInArgs;
-    uint32_t setMcastOutArgs;
-
-    if (Enet_isCpswFamily(enetType))
-    {
-        Enet_Handle hEnet = Enet_getHandle(enetType, instId);
-
-        EnetAppUtils_assert(hEnet != NULL);
-        memset(&setMcastInArgs, 0, sizeof(setMcastInArgs));
-        memcpy(&setMcastInArgs.addr.addr[0U], testMCastAddr,
-               sizeof(setMcastInArgs.addr.addr));
-        setMcastInArgs.addr.vlanId  = 0;
-        setMcastInArgs.info.super = false;
-        setMcastInArgs.info.numIgnBits = 0;
-        setMcastInArgs.info.fwdState = CPSW_ALE_FWDSTLVL_FWD;
-        setMcastInArgs.info.portMask = portMask;
-        ENET_IOCTL_SET_INOUT_ARGS(&prms, &setMcastInArgs, &setMcastOutArgs);
-        ENET_IOCTL(hEnet, coreId, CPSW_ALE_IOCTL_ADD_MCAST, &prms, status);
-        if (status != ENET_SOK)
-        {
-           EnetAppUtils_print("EnetTestBcastMcastLimit_AddAleEntry() failed CPSW_ALE_IOCTL_ADD_MCAST: %d\n",
-                               status);
-        }
-    }
-}
-
-
 static inline int32_t App_isNetworkUp(struct netif* netif_)
 {
     return (netif_is_up(netif_) && netif_is_link_up(netif_) && !ip4_addr_isany_val(*netif_ip4_addr(netif_)));
 }
+
 
 static void App_netifStatusChangeCb(struct netif *pNetif)
 {
@@ -134,128 +101,6 @@ static void App_netifLinkChangeCb(struct netif *pNetif)
         DebugP_log("Network Link DOWN Event\r\n");
     }
     return;
-}
-
-static void EnetApp_portLinkStatusChangeCb(Enet_MacPort macPort,
-                                           bool isLinkUp,
-                                           void *appArg)
-{
-    EnetAppUtils_print("MAC Port %u: link %s\r\n",
-                       ENET_MACPORT_ID(macPort), isLinkUp ? "up" : "down");
-}
-
-
-static void EnetApp_mdioLinkStatusChange(Cpsw_MdioLinkStateChangeInfo *info,
-                                         void *appArg)
-{
-    if (info->linkChanged)
-    {
-        EnetAppUtils_print("Link Status Changed. PHY: 0x%x, state: %s\r\n",
-                info->phyAddr,
-                info->isLinked? "up" : "down");
-    }
-}
-
-
-void EnetApp_initLinkArgs(Enet_Type enetType,
-                          uint32_t instId,
-                          EnetPer_PortLinkCfg *linkArgs,
-                          Enet_MacPort macPort)
-{
-    EnetPhy_Cfg *phyCfg = &linkArgs->phyCfg;
-    EnetMacPort_LinkCfg *linkCfg = &linkArgs->linkCfg;
-    EnetMacPort_Interface *mii = &linkArgs->mii;
-    EnetBoard_EthPort ethPort;
-    const EnetBoard_PhyCfg *boardPhyCfg;
-    int32_t status;
-
-    /* Setup board for requested Ethernet port */
-    ethPort.enetType = enetType;
-    ethPort.instId   = instId;
-    ethPort.macPort  = macPort;
-    ethPort.boardId  = EnetBoard_getId();
-
-    /* Get the Mii config for Ethernet port */
-    EnetBoard_getMiiConfig(&ethPort.mii);
-
-    status = EnetBoard_setupPorts(&ethPort, 1U);
-    EnetAppUtils_assert(status == ENET_SOK);
-
-    if (Enet_isCpswFamily(ethPort.enetType))
-    {
-        CpswMacPort_Cfg *macCfg = (CpswMacPort_Cfg *)linkArgs->macCfg;
-        CpswMacPort_initCfg(macCfg);
-        if (EnetMacPort_isSgmii(mii) || EnetMacPort_isQsgmii(mii))
-        {
-            macCfg->sgmiiMode = ENET_MAC_SGMIIMODE_SGMII_WITH_PHY;
-        }
-        else
-        {
-            macCfg->sgmiiMode = ENET_MAC_SGMIIMODE_INVALID;
-        }
-    }
-
-    boardPhyCfg = EnetBoard_getPhyCfg(&ethPort);
-    if (boardPhyCfg != NULL)
-    {
-        EnetPhy_initCfg(phyCfg);
-        phyCfg->phyAddr     = boardPhyCfg->phyAddr;
-        phyCfg->isStrapped  = boardPhyCfg->isStrapped;
-        phyCfg->loopbackEn  = false;
-        phyCfg->skipExtendedCfg = boardPhyCfg->skipExtendedCfg;
-        phyCfg->extendedCfgSize = boardPhyCfg->extendedCfgSize;
-        memcpy(phyCfg->extendedCfg, boardPhyCfg->extendedCfg, phyCfg->extendedCfgSize);
-
-    }
-    else
-    {
-        DebugP_log("No PHY configuration found for MAC port %u\r\n",
-                           ENET_MACPORT_ID(ethPort.macPort));
-        EnetAppUtils_assert(false);
-    }
-
-    mii->layerType     = ethPort.mii.layerType;
-    mii->sublayerType  = ethPort.mii.sublayerType;
-    mii->variantType   = ENET_MAC_VARIANT_FORCED;
-    linkCfg->speed     = ENET_SPEED_AUTO;
-    linkCfg->duplexity = ENET_DUPLEX_AUTO;
-
-    if (Enet_isCpswFamily(ENET_CPSW_3G))
-    {
-        CpswMacPort_Cfg *macCfg = (CpswMacPort_Cfg *)linkArgs->macCfg;
-
-        if (EnetMacPort_isSgmii(mii) || EnetMacPort_isQsgmii(mii))
-        {
-            macCfg->sgmiiMode = ENET_MAC_SGMIIMODE_SGMII_WITH_PHY;
-        }
-        else
-        {
-            macCfg->sgmiiMode = ENET_MAC_SGMIIMODE_INVALID;
-        }
-    }
-}
-
-void EnetApp_updateCpswInitCfg(Enet_Type enetType, uint32_t instId, Cpsw_Cfg *cpswCfg)
-{
-#if defined (ENET_SOC_HOSTPORT_DMA_TYPE_CPDMA)
-    EnetCpdma_Cfg * dmaCfg = (EnetCpdma_Cfg *)cpswCfg->dmaCfg;
-
-    EnetAppUtils_assert(dmaCfg != NULL);
-    EnetAppUtils_assert(EnetAppUtils_isDescCached() == false);
-    dmaCfg->rxInterruptPerMSec = 8;
-    dmaCfg->txInterruptPerMSec = 2;
-#endif
-
-
-#if (ENET_SYSCFG_ENABLE_MDIO_MANUALMODE == 1U)
-    cpswCfg->mdioLinkStateChangeCb    = NULL;
-    cpswCfg->mdioLinkStateChangeCbArg = NULL;
-#else
-    cpswCfg->mdioLinkStateChangeCb    = &EnetApp_mdioLinkStatusChange;
-    cpswCfg->mdioLinkStateChangeCbArg = NULL;
-#endif
-    cpswCfg->portLinkStatusChangeCb = &EnetApp_portLinkStatusChangeCb;
-    cpswCfg->portLinkStatusChangeCbArg = NULL;
 }
 
 
